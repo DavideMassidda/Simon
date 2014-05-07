0b2bce904c014cb5242c8aa94311a5a39041e2e9
DavideMassidda <davide.massidda@gmail.com>
2014-05-07 14:54:24 +0200

    release 0.0

---
+++ main.py
@@ -0,0 +1,164 @@
+﻿# -*- coding: utf-8 -*-
+from __future__ import division
+from psychopy import visual, core, event
+from settings import * # Carica la maschera per le impostazioni
+
+# --------------------------------------
+# Impostazioni esprimento
+# --------------------------------------
+
+buttons = ['l','a']         # Pulsanti di risposta (L:Dx,A:Sx)
+subjNum = SubjCode.get()    # Codice numerico del soggetto
+seq = FirstSeq.get()        # Codice sequenza di start
+isTrain = IsTrain.get()     # Indica se registrare le risposte
+ISI =  Isi.get()            # Intervallo inter-stimolo (sec)
+
+# --------------------------------------
+# Impostazioni monitor
+# --------------------------------------
+
+screenSize = (MonitorResW.get(),MonitorResH.get())  # Risoluzione (w,h)
+fullscreen = FS.get()                               # Schermo intero si/no
+screenColor = ScreenCol.get()                       # Colore finestra
+stimColor = StimCol.get()                           # Colore stimolo
+
+# --------------------------------------
+# Organizzazione stimoli
+# --------------------------------------
+# Fattori sperimentali:
+# Stim: stimolo (Cerchio vs Quadrato)
+# Side: lato di presentazione (Dx vs Sx)
+
+if isTrain == 0: # Non e' una sessione di prova
+    Stim = [
+        [0, 0, 1, 1],
+        [0, 0, 1, 1],
+        [1, 1, 0, 0],
+        [1, 1, 0, 0],
+        [0, 1, 0, 1],
+        [1, 0, 1, 0],
+        [0, 1, 0, 1],
+        [1, 0, 1, 0]]
+    Side = [
+        [0, 1, 0, 1],
+        [1, 0, 1, 0],
+        [0, 1, 0, 1],
+        [1, 0, 1, 0],
+        [0, 0, 1, 1],
+        [0, 0, 1, 1],
+        [1, 1, 0, 0],
+        [1, 1, 0, 0]]
+else: # E' una sessione di prova
+    seq = 1 # Si parte comunque dalla prima sequenza
+    Stim = [
+        [0, 1, 0, 1],
+        [0, 1, 0, 1]]
+    Side = [
+        [1, 0, 0, 1],
+        [0, 1, 1, 0]]
+numStim = len(Stim) # Numero di stimoli da presentare
+
+# --------------------------------------
+# Costruzione finestra
+# --------------------------------------
+
+screen = visual.Window(size=screenSize,units="pix",fullscr=fullscreen,color=screenColor) # Finestra
+screen.setMouseVisible(False) # Mouse
+fixation = visual.TextStim(screen,text="+",color=stimColor,height=25,pos=(0,0)) # Punto di fissazione
+screen.flip() # Avvio interfaccia
+
+# --------------------------------------
+# Istruzioni
+# --------------------------------------
+
+textWrapper = [0,0] # Dimensioni del contenitore di testo
+textWrapper[0] = round(screenSize[0]*0.75)
+textWrapper[1] = round(screenSize[1]*0.1)
+InstrFile = open('instructions.txt','r')
+instrText = InstrFile.read()
+InstrFile.close()
+instrStim = visual.TextStim(screen,text=instrText,color=stimColor,height=25,pos=(0,textWrapper[1]),wrapWidth=textWrapper[0])
+instrStim.draw()
+screen.flip()
+event.waitKeys(keyList='space') # Attende che venga premuta la barra spaziatrice
+
+# --------------------------------------
+# Inizio esperimento
+# --------------------------------------
+
+alertText = "Poni l'indice sinistro sul pulsante " + buttons[1].upper() + " e l'indice destro sul pulsante " + buttons[0].upper() + ".\n\n"
+alertText = alertText + "La prova comincera\' tra pochi secondi."
+alertStim = visual.TextStim(screen,text=alertText,color=stimColor,height=30,pos=(0,textWrapper[1]),wrapWidth=textWrapper[0])
+alertStim.draw()
+screen.flip()
+core.wait(7)
+
+# --------------------------------------
+# Avvio sessione
+# --------------------------------------
+
+screen.flip()           # Avvia uno schermo pulito
+count = 0               # Conta quante sequenze sono state presentate
+seq = seq-1             # Perche' il conteggio degli indici parte da 0 e non da 1
+timer = core.Clock()    # Timer per tempi di reazione
+while count < numStim:  # Inizia la presentazione degli stimoli
+    count = count+1
+    for i in range(4):
+        fixation.draw()     # Disegna il punto di fissazione
+        screen.flip()       # Riavvia la finestra
+        core.wait(ISI)      # Blocca l'esecuzione per ISI
+        event.clearEvents() # Ripulisce tutte le precedenti registrazioni di pulsanti
+        if Stim[seq][i] == 0: # Cerchio
+            exactKey = buttons[0] # Pulsante di risposta corretto
+            if Side[seq][i] == 0:
+                # Cerchio a sinistra
+                visual.Circle(screen,radius=80,pos=(-300,0),fillColor=stimColor,lineColor=stimColor,edges=256).draw() # interpolate=True
+            else:
+                # Cerchio a destra
+                visual.Circle(screen,radius=80,pos=(300,0),fillColor=stimColor,lineColor=stimColor,edges=256).draw() # interpolate=True
+        else: # Quadrato
+            exactKey = buttons[1] # Pulsante di risposta corretto
+            if Side[seq][i] == 0:
+                # Quadrato a sinistra
+                visual.Rect(screen,width=160,height=160,pos=(-300,0),fillColor=stimColor,lineColor=stimColor).draw()
+            else:
+                # Quadrato a destra
+                visual.Rect(screen,width=160,height=160,pos=(300,0),fillColor=stimColor,lineColor=stimColor).draw()
+        fixation.draw()         # Disegna il punto di fissazione
+        screen.flip()           # Riavvia la finestra
+        timer.reset()           # Azzera il timer
+        t0 = timer.getTime()    # Avvia il timer
+        respKey = event.waitKeys(keyList=buttons) # Attende la risposta
+        t1 = timer.getTime()    # Blocca il timer
+        if isTrain == 0:        # Se si deve registrare la risposta
+            respTime = (t1-t0)*1000 # Calcolo tempi di reazione
+            respTime = str(respTime)
+            respTime = respTime.replace('.',',') # Sostituzione punti in virgoole nel decimale per csv2
+            score = respKey[0] == exactKey # Calcolo score
+            if score == True:
+                score = 1
+            else:
+                score = 0
+            # Registrazione dati su file
+            dataFile = open('data/expdata.csv','a')
+            dataFile.write("%s;%s;%s;%s;%s;%s\n"%(subjNum,Stim[seq][i]+1,Side[seq][i]+1,count,score,respTime))
+            dataFile.close()
+    # Si aggiorna il valore di seq
+    seq = seq+1
+    if seq == numStim:
+        seq = 0
+
+# --------------------------------------
+# Conclusione esperimento
+# --------------------------------------
+
+fixation.draw()
+screen.flip()
+core.wait(1)
+alertText = "Prova conclusa\n\nGrazie per la partecipazione"
+alertStim = visual.TextStim(screen,text=alertText,color=stimColor,height=30,pos=(0,200),wrapWidth=800)
+alertStim.draw()
+screen.flip()
+core.wait(3)
+screen.close()
+core.quit()
---
+++ settings.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+from Tkinter import *
+import tkFont
+
+# Toplevel
+maskInfo = Tk()
+maskInfo.wm_title("")
+
+# Frame
+infoFrame0 = Frame(maskInfo,padx=5,pady=5,bg="#5a5a5a",width=500,height=40)
+infoFrame1 = Frame(maskInfo,padx=5,pady=10)
+infoFrame1 = Frame(maskInfo,padx=15,pady=10)
+infoFrame3 = Frame(maskInfo,padx=15,pady=10)
+buttonFrame = Frame(maskInfo,pady=5,height=40)
+
+# Font dei titoli di sezione
+sectionFont = tkFont.Font(family='sans serif',weight='bold')
+
+# ------------------------------------
+# Intestazione
+# ------------------------------------
+font = tkFont.Font(family='sans serif',weight='bold',size=14)
+titleLabel = Label(infoFrame0,text="",font=font,bg="#5a5a5a",fg="white")
+
+# ------------------------------------
+# Impostazioni soggetto
+# ------------------------------------
+subjectLabel = Label(infoFrame1,text="Subject information",font=sectionFont)
+
+# Variabili
+SubjCode = IntVar()
+Age = IntVar()
+Year = IntVar()
+Sex = StringVar()
+
+# Codice soggetto
+subjCodeLabel = Label(infoFrame1,text="Code")
+subjCodeCell  = Entry(infoFrame1,textvariable=SubjCode,bg="#ffff60",width=9,cursor="xterm")
+# Anno di nascita
+yearLabel = Label(infoFrame1,text="Year of birth")
+yearCell  = Entry(infoFrame1,textvariable=Year,bg="white",width=9,cursor="xterm")
+# Anni compiuti
+ageLabel = Label(infoFrame1,text="Age")
+ageCell  = Entry(infoFrame1,textvariable=Age,bg="white",width=9,cursor="xterm")
+# Sesso
+sexLabel = Label(infoFrame1,text="Sex")
+sexF = Radiobutton(infoFrame1,text="Female",value="F",variable=Sex)
+sexM = Radiobutton(infoFrame1,text="Male",value="M",variable=Sex)
+# sexM.select()
+
+# ------------------------------------
+# Impostazioni monitor/esperimento
+# ------------------------------------
+settingsLabel = Label(infoFrame1,text="Experiment settings",font=sectionFont)
+
+# Variabili
+MonitorResW = IntVar()
+MonitorResW.set(1366)
+MonitorResH = IntVar()
+MonitorResH.set(768)
+ScreenCol = StringVar()
+ScreenCol.set('#b4b4b4')
+StimCol = StringVar()
+StimCol.set('#000000')
+FS = IntVar()
+FS.set(1)
+FirstSeq = IntVar()
+FirstSeq.set(0)
+Isi = DoubleVar()
+Isi.set(1.5)
+IsTrain = IntVar()
+IsTrain.set(0)
+
+# Risoluzione monitor (base)
+reswLabel = Label(infoFrame1,text="Resolution width (pixel)")
+reswCell  = Entry(infoFrame1,textvariable=MonitorResW,bg="white",width=9,cursor="xterm")
+# Risoluzione monitor (altezza)
+reshLabel = Label(infoFrame1,text="Resolution height (pixel)")
+reshCell  = Entry(infoFrame1,textvariable=MonitorResH,bg="white",width=9,cursor="xterm")
+# Colore sfondo
+screenColLabel = Label(infoFrame1,text="Background color")
+screenColCell  = Entry(infoFrame1,textvariable=ScreenCol,bg="white",width=9,cursor="xterm")
+# Colore linea
+stimColLabel = Label(infoFrame1,text="Stimuli color")
+stimColCell  = Entry(infoFrame1,textvariable=StimCol,bg="white",width=9,cursor="xterm")
+# Fullscreen
+fullscreenBut = Checkbutton(infoFrame1,text="Full screen",variable=FS)
+# Inter stimulus interval
+isiLabel = Label(infoFrame1,text="ISI")
+isiCell  = Entry(infoFrame1,textvariable=Isi,bg="white",width=9,cursor="xterm")
+# Sequenza di partenza
+seqLabel = Label(infoFrame1,text="First sequence")
+seqCell  = Entry(infoFrame1,textvariable=FirstSeq,bg="#ffff60",width=9,cursor="xterm")
+# Sessione di prova
+istrialBut = Checkbutton(infoFrame1,text="Training session",variable=IsTrain)
+
+# ------------------------------------
+# Pulsante
+# ------------------------------------
+goMain = 0
+# Se goMain rimane uguale a zero alla fine dello script, l'applicazione verra' chiusa.
+# Questo serve perche', se la maschera info viene chiusa dal pulsante x, quindi senza
+# premere il pulsante send, la successiva esecuzione del programma non deve avvenire.
+def WriteInfo():
+    global maskInfo,SubjCode,goMain,IsTrain
+    goAhead = 0
+    if IsTrain.get() == 1:
+        goAhead = 1
+    else:
+        if SubjCode.get() != 0 and FirstSeq.get() != 0:
+            dataFile = open("data/subjects.csv","a")
+            dataFile.write("%g;%g;%g;%s;%g\n"%(SubjCode.get(),Year.get(),Age.get(),Sex.get(),FirstSeq.get()))            
+            dataFile.close()
+            goAhead = 1
+    if goAhead == 1 or IsTrain == 1:
+        maskInfo.quit()
+        goMain = 1
+
+infoBut = Button(buttonFrame,text="START",default="active",bg="#5a5a5a",fg="white",command=WriteInfo)
+
+# ------------------------------------
+# Costruzione interfaccia
+# ------------------------------------
+
+infoFrame0.pack()
+infoFrame0.grid_propagate(0)
+titleLabel.grid(row=1,column=1,columnspan=3)
+
+infoFrame1.pack()
+# Impostazioni soggetto
+subjectLabel.grid(row=1,column=1,columnspan=2)
+subjCodeLabel.grid(row=2,column=1,sticky="e")
+subjCodeCell.grid(row=2,column=2,sticky="w")
+yearLabel.grid(row=3,column=1,sticky="e")
+yearCell.grid(row=3,column=2,sticky="w")
+ageLabel.grid(row=4,column=1,sticky="e")
+ageCell.grid(row=4,column=2,sticky="w")
+sexM.grid(row=5,column=1,sticky="e")
+sexF.grid(row=5,column=2,sticky="e")
+# Impostazioni monitor/esperimento
+settingsLabel.grid(row=1,column=3,columnspan=2)
+reswLabel.grid(row=2,column=3,sticky="e")
+reswCell.grid(row=2,column=4,sticky="w")
+reshLabel.grid(row=3,column=3,sticky="e")
+reshCell.grid(row=3,column=4,sticky="w")
+screenColLabel.grid(row=4,column=3,sticky="e")
+screenColCell.grid(row=4,column=4,sticky="w")
+stimColLabel.grid(row=5,column=3,sticky="e")
+stimColCell.grid(row=5,column=4,sticky="w")
+fullscreenBut.grid(row=6,column=3,columnspan=2,sticky="e")
+isiLabel.grid(row=7,column=3,sticky="e")
+isiCell.grid(row=7,column=4,sticky="w")
+seqLabel.grid(row=8,column=3,sticky="e")
+seqCell.grid(row=8,column=4,sticky="w")
+istrialBut.grid(row=9,column=3,columnspan=2,sticky="e")
+
+# Pulsante avvio sessione
+buttonFrame.pack()
+infoBut.grid()
+
+# Loop e chiusura maschera
+maskInfo.mainloop()
+if goMain == 0:
+    quit()
+else:
+    maskInfo.destroy()
